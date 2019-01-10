0ba5b8c14127d6c5652a9eb97e26c746393ff5e6
Michal <michal.kowalski.wro@gmail.com>
2019-01-10 10:04:15 +0100

    Updates

---
+++ precache-manifest.5143a563c0ee84e551376719a91705cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "6d6a8381d63b8626c893",
+    "url": "/todo-app/static/js/main.6d6a8381.chunk.js"
+  },
+  {
+    "revision": "07140812a98c93f90dd2",
+    "url": "/todo-app/static/js/1.07140812.chunk.js"
+  },
+  {
+    "revision": "6d6a8381d63b8626c893",
+    "url": "/todo-app/static/css/main.82133118.chunk.css"
+  },
+  {
+    "revision": "e68616a8e5ad2f8b6d3fec5430fc2470",
+    "url": "/todo-app/index.html"
+  }
+];
